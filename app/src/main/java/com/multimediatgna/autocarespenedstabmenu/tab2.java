3ac40840a701b52d81be8fd91c3f62898a10c405
Gabriel Puig Martín <gpuig22@xtec.cat>
2022-02-25 13:32:46 +0100

    tabuladores "EMPRESA" y "CONTACTE" completados

--- app/src/main/java/com/multimediatgna/autocarespenedstabmenu/tab2.java
+++ app/src/main/java/com/multimediatgna/autocarespenedstabmenu/tab2.java
@@ -1,12 +1,24 @@
 package com.multimediatgna.autocarespenedstabmenu;
 
+import static com.multimediatgna.autocarespenedstabmenu.R.id.myimageview;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,10 +27,12 @@ import android.view.ViewGroup;
  */
 public class tab2 extends Fragment {
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private CardView mycardview;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -36,19 +50,22 @@ public class tab2 extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment tab2.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static tab2 newInstance(String param1, String param2) {
         tab2 fragment = new tab2();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+        Log.i("Test", "tab2 newInstance() executed");
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +75,57 @@ public class tab2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        View myview = inflater.inflate(R.layout.fragment_tab2, container, false);
+       mycardview = myview.findViewById(R.id.mycontacte);
+
+        mycardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (Build.VERSION.SDK_INT < 23) {
+                    phoneCall();
+                }else {
+
+                    if (ActivityCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                        phoneCall();
+                    }else {
+                        final String[] PERMISSIONS_STORAGE = {Manifest.permission.CALL_PHONE};
+                        //Asking request Permissions
+                        ActivityCompat.requestPermissions(getActivity(), PERMISSIONS_STORAGE, 9);
+                    }
+                }
+
+            }
+        });
+        return myview;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        boolean permissionGranted = false;
+        switch(requestCode){
+            case 9:
+                permissionGranted = grantResults[0]== PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if(permissionGranted){
+            phoneCall();
+        }else {
+            Toast.makeText(getActivity(), "You don't assign permission.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void phoneCall(){
+        String myphonenumber =  getString(R.string.callNumber);
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse(myphonenumber));
+            getActivity().startActivity(callIntent);
+        }else{
+            Toast.makeText(getActivity(), "You don't assign permission.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
